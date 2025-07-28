bf5cdc7cc295f0952324fe7d3d0a50be77b315e7
rinshafebin <rinshafebinkk12@gmail.com>
2025-07-28 14:25:21 +0530

    updated login,register,home paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Navigation, Navigate } from 'react-router-dom'
+import Login from './pages/Login'
+import Home from './pages/Home'
+import Register from './pages/Register'
+import NotFound from './pages/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+
+function Logout() {
+  localStorage.clear()
+  return <Navigate to='/login' />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <route path='/' element={<ProtectedRoute> <Home /></ProtectedRoute>} />
+          <route path='/logim' element={<Login />} />
+          <route path='/register' element={<Register />} />
+          <route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+
+    </div>
   )
 }
 
